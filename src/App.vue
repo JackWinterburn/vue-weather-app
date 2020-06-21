60a1e540a1ac3e80b1aa67e29f14bbf848405837
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-06-21 19:15:53 +0100

    changed styling around searchabar

--- src/App.vue
+++ src/App.vue
@@ -131,7 +131,6 @@ main{
 .search-box .search-bar:focus{
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 0;
 }
 
 .location-box .location{
